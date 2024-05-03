8e931ea3a524ba5394048099b4d8d7af790c354d
Rick Huang <rick.liyue.huang@gmail.com>
2024-05-03 11:34:11 +1000

    ci: create the new project

---
+++ src/components/LikeButton.tsx
@@ -0,0 +1,9 @@
+import React, { useState } from 'react'
+
+export const LikeButton: React.FC = () => {
+    const [like, setLike] = useState<number>(0);
+
+  return (
+    <div>LikeButton</div>
+  )
+}
