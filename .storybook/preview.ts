231ee158144508684d26cdae139f7ad2a8785dc2
Rick Huang <RickHuang@rauland.com.au>
2024-05-06 15:42:06 +1000

    feat: create task and tasklist

--- .storybook/preview.ts
+++ .storybook/preview.ts
@@ -1,7 +1,9 @@
 import type { Preview } from "@storybook/react";
+import '../src/index.css';
 
 const preview: Preview = {
   parameters: {
+    actions: { argTypesRegex: "^on[A-Z].*" }, // configures storybook to log the actions --- onArchiveTask, onPinTask 
     controls: {
       matchers: {
         color: /(background|color)$/i,
